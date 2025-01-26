867b6d36893b114e0c36d3ed9d679e4d8f46267b
Dylan <safirodylan@gmail.com>
2025-01-26 15:17:01 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -6,3 +6,14 @@ products.push("Oreo McFlurry"); // Adds new product to array
 
 products.pop(); // Removes last product from array
     console.log("Updated Inventory:", products);  
+
+
+// Task 2: Student Scores
+let scores = [82, 51, 63, 75, 57]; // Initial Scores
+
+scores[1] = 91 // Updates second value in array
+    console.log("Updated Scores:", scores) // Updated Scores
+
+let totalScores = scores.reduce((total, amount) => total + amount, 0); // Calculates total of scores
+let averageScores = totalScores / scores.length // Calculates average of scores
+    console.log("Average Score:", averageScores); // Average Score
