99489fac9e2460e3f01f99f7d1b58423054556ce
Dylan <safirodylan@gmail.com>
2025-01-26 14:39:29 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,8 @@
+// Task 1: Store Inventory
+let products = ["Big Mac", "Fries", "Soft Drink", "Chicken Nuggets", "Chicken Sandwich"]; // Initial Products Inventory
+
+products.push("Oreo McFlurry"); // Adds new product to array
+    console.log("Inventory:", products);
+
+products.pop(); // Removes last product from array
+    console.log("Updated Inventory:", products);  
